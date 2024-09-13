5b52acc99633622ff2755be3d7fbc6929571093a
Matthew Webster <contactmattwebster@gmail.com>
2024-09-13 11:05:58 +0000

    admin panel power up

--- codestar/settings.py
+++ codestar/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-]
+    'django_summernote',
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
