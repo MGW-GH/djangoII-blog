1f26ec9a2124b331204e976e97574b712c8f50fe
Matthew Webster <contactmattwebster@gmail.com>
2024-09-17 07:58:44 +0000

    added signup page coontent

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+
 ALLOWED_HOSTS = ['8000-mgwgh-djangoiiblog-mra60ssmc2q.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
